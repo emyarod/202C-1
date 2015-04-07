9ba31c0f7934fcfeef3e64734f87f135025d1900
emyarod <allllaboutyou@gmail.com>
2015-04-07 17:56:31 -0400

    add sidebar behavior

--- js/script.js
+++ js/script.js
@@ -16,6 +16,37 @@
 // });
 
 $('body').scrollspy({
-    target: '.bs-docs-sidebar',
-    offset: 40
+  target: '.bs-docs-sidebar',
+  offset: 40
+});
+
+$("#nav ul li a[href^='#']").on('click', function(e) {
+	// prevent default anchor click behavior
+	e.preventDefault();
+
+	// store hash
+	var hash = this.hash;
+
+	// animate
+	$('html, body').animate({
+		scrollTop: $(hash).offset().top
+	}, 300, function(){
+		// when done, add hash to url
+  	// (default click behaviour)
+  	window.location.hash = hash;
+	});
+});
+
+$('.easy-sidebar-toggle').click(function(e) {
+  e.preventDefault();
+  $('.easy-sidebar-container').toggleClass('toggled');
+  $('.navbar.easy-sidebar').toggleClass('toggled');
+});
+$('html').on('swiperight', function(){
+  $('.easy-sidebar-container').addClass('toggled');
+  $('.navbar.easy-sidebar').addClass('toggled');
+});
+$('html').on('swipeleft', function(){
+	$('.easy-sidebar-container').removeClass('toggled');
+  $('.navbar.easy-sidebar').removeClass('toggled');
 });
